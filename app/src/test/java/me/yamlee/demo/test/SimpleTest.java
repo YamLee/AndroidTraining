4f164497a6f93aa4d99bb5da1a7be36c98dff29d
YamLee <liyaninhn@gmail.com>
2015-12-02 19:18:10 +0800

    gradle configuration

--- app/src/test/java/me/yamlee/demo/test/SimpleTest.java
+++ app/src/test/java/me/yamlee/demo/test/SimpleTest.java
@@ -15,20 +15,6 @@ import yamlee.com.demo.BuildConfig;
 public class SimpleTest {
     @Test
     public void testSimple() {
-//        assertThat(true, is(false));
-//        assertThat(test,hasProperty("test"));
-//        String test = "test";
-//        anything("anything will match");
-//        int result = 1 + 2;
-//        assertThat(result, describedAs("A integer should equals %0 ", equalTo(2), 2));
-//        assertThat(result,allOf());
-//        assertThat(2,hasToString("3"));
-//        assertThat(1.99,closeTo(2.0,1.0));
-//        assertThat(null,notTextEmpty());
-        try {
-            XmlConfigurator.main(new String[]{"custom_matchers.xml"});
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 }
